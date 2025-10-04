0c9c26a1b9f9b2fc6fa19a6e0164d72a66db7fea
jibrilMaygag <jibrilmeygag@gmail.com>
2025-10-04 07:49:50 -0400

    finish 0x04-Typescript

---
+++  0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,16 @@
+interface MajorCredits {
+_majorBrand: "MajorCredits";
+credits:number
+}
+interface MinorCredits{
+    _minorBrand: "MinorCredits";
+    credits:number
+
+}
+const sumMajorCredits=function(subject1:MajorCredits,subject2:MajorCredits){
+    return subject1.credits+subject2+credits
+
+}
+const sumMinorCredits=function(subject1:MinorCredits,subject2:MinorCredits){
+    
+}
